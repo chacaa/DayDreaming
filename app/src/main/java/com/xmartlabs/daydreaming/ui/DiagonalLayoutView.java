260e3a2818fe3a35513bd9c129be74c13661d771
santiago <santiagocasaslopez@gmail.com>
2017-04-14 00:26:08 -0300

    Adds trending screen feature.

--- app/src/main/java/com/xmartlabs/daydreaming/ui/DiagonalLayoutView.java
+++ app/src/main/java/com/xmartlabs/daydreaming/ui/DiagonalLayoutView.java
@@ -13,7 +13,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.github.florent37.diagonallayout.DiagonalLayout;
 import com.xmartlabs.daydreaming.R;
 
 import butterknife.BindView;
