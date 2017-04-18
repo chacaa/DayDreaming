e36a97fd031da7b5862c8197ad5647d0d7e996c2
santiago <santiagocasaslopez@gmail.com>
2017-04-18 01:13:11 -0300

    .

--- app/src/main/java/com/xmartlabs/daydreaming/ui/DiagonalLayoutView.java
+++ app/src/main/java/com/xmartlabs/daydreaming/ui/DiagonalLayoutView.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.github.florent37.diagonallayout.DiagonalLayout;
 import com.xmartlabs.daydreaming.R;
 
 import butterknife.BindView;
