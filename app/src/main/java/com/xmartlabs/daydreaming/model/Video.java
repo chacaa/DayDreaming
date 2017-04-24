fefcbc942398fbce9fcedfae7c5751db3adc3abc
chaca <scasas@xmartlabs.com>
2017-04-24 12:12:41 -0300

    Some changes requested during the PR

--- app/src/main/java/com/xmartlabs/daydreaming/model/Video.java
+++ app/src/main/java/com/xmartlabs/daydreaming/model/Video.java
@@ -1,12 +1,7 @@
 package com.xmartlabs.daydreaming.model;
 
-import org.parceler.Parcel;
-
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 /**
  * Created by chaca on 4/21/17.
@@ -14,11 +9,7 @@ import lombok.RequiredArgsConstructor;
 @Builder
 @Data
 public class Video {
-  String id;
-  String theme;
-  String type;
-
-  public String getId() {
-    return id;
-  }
+  private String id;
+  private String theme;
+  private String type;
 }
